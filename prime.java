e1c87a804a737ea3a2418a2b5913b2429bf48e7d
Akhil Ramachandran <akhil2109@gmail.com>
2018-10-27 15:37:50 +0530

    Program to check whether the number is prime or not
    Signed-off-by: Akhil Ramachandran <akhil2109@gmail.com>

---
+++ prime.java
@@ -0,0 +1,29 @@
+//Program to check to whether the number is prime or not in java    
+import java.io.*;
+class primenumber
+{
+	public static void main()
+	{
+		int m=0,i,flag=0;
+		m=n/2
+		if(n==0||n==1)
+		{
+			System.out.println(n+"is not prime number");
+		}
+		else
+		{
+			for(i=2;i<=m;i++)
+			{
+				if(n%i==0)
+				{
+					Sysytem.out.println(n+"is not prime number);
+					flag=1;
+					break;
+				}
+			}
+		if( flag==0)
+		{
+			System.out.println(n+"is prime number);
+		}
+	}
+}
